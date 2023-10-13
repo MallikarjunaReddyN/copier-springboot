1a49552b437d9ef70c9a90e32bd64dd12185c41c
Mallikarjuna Reddy Nimmakayala <mallikarjunareddy.nimmakayala@gmail.com>
2023-10-13 19:21:39 +0530

    added base entity for database

---
+++ template/src/main/java/{{ group_id.replace('.', _copier_conf.sep) }}{{ _copier_conf.sep }}/{{project_package}}/domain/{% if is_db_required == 'Yes' %}entity{% endif %}/BaseEntity.java
@@ -0,0 +1,42 @@
+package com.malli.{{ project_package }}.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(updatable = false, nullable = false)
+    private UUID id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BaseEntity)) return false;
+        BaseEntity that = (BaseEntity) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity {" +
+                "id = " + id +
+                "}";
+    }
+}
